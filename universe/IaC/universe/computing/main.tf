e377e76f50da21f795ec5c982f672e822c311fae
moeid heidari <moeidheidari@comfortel.pro>
2023-01-22 21:35:08 +0300

    universe virtual machine variables and virtual machine 1 implementation added

--- universe/IaC/universe/computing/main.tf
+++ universe/IaC/universe/computing/main.tf
@@ -1,16 +1,102 @@
-#==============================================================================================================================#
-#                                                                                                                              #
-#                                                      Dipal Server Group                                                      #
-#                                                                                                                              #
-#==============================================================================================================================#
-resource "kubernetes_manifest" "ubuntu-terraform" {
+#==============================================================================#
+#                                                                              #
+#                                 MIT License                                  #
+#                                                                              #
+#                       Copyright (c) 2023 Skyfarm                             #
+#                                                                              #
+# Permission is hereby granted, free of charge, to any person obtaining a copy #
+#of this software and associated documentation files (the Software), to deal #
+# in the Software without restriction, including without limitation the rights #
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell   #
+#    copies of the Software, and to permit persons to whom the Software is     #
+#           furnished to do so, subject to the following conditions:           #
+#                                                                              #
+#The above copyright notice and this permission notice shall be included in all#
+#               copies or substantial portions of the Software.                #
+#                                                                              #
+#  THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR  #
+#   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,   #
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE  #
+#    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER    #
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, #
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE #
+#                                  SOFTWARE.                                   #
+#                                                                              #
+#==============================================================================#
+#==============================================================================#
+#                                                                              #
+#                             System Server Group                              #
+#                                                                              #
+#==============================================================================#
+
+#-------------------------------------------------------------------------------
+# creates the name space for for system
+resource "kubernetes_namespace" "system_namespace" {
+  metadata {
+    name = var.system_namespace
+  }
+}
+#-------------------------------------------------------------------------------
+# create persistent volumes
+resource "kubernetes_persistent_volume" "system_vm1_persistent_volume" {
+  metadata {
+    name = "${var.system_vm_name}-persistent-volume"
+  }
+  spec {
+    node_affinity {
+      required {
+        node_selector_term {
+          match_expressions {
+            key      = "beta.kubernetes.io/os"
+            operator = "In"
+            values   = ["linux"]
+          }
+        }
+      }
+    }
+    storage_class_name = ""
+    access_modes       = ["ReadWriteMany"]
+    capacity = {
+      storage = "10Gi"
+    }
+    persistent_volume_source {
+      local {
+        path = "/mnt/disk1"
+      }
+    }
+  }
+}
+#-------------------------------------------------------------------------------
+#Create persistent volume claim
+resource "kubernetes_persistent_volume_claim" "system_vm1_persistent_volume_claim" {
+  depends_on = [
+    kubernetes_persistent_volume.system_vm1_persistent_volume
+  ]
+  metadata {
+    name      = "${var.system_vm_name}-persistent-volume-claim"
+    namespace = var.system_namespace
+  }
+
+  spec {
+
+    access_modes = ["ReadWriteMany"]
+    resources {
+      requests = {
+        storage = "10Gi"
+      }
+    }
+  }
+}
+#-------------------------------------------------------------------------------
+# Creates virtual machine 1
+resource "kubernetes_manifest" "ubuntu_terraform" {
   manifest = {
     apiVersion = "kubevirt.io/v1"
-    kind = "VirtualMachine"
+    kind       = "VirtualMachine"
     metadata = {
       creationTimestamp = null
-      name = "ubuntu"
-      namespace = var.namespace
+      name              = var.system_vm_name
+      namespace         = var.system_namespace
     }
     spec = {
       runStrategy = "Always"
@@ -34,12 +120,18 @@ resource "kubernetes_manifest" "ubuntu-terraform" {
                   }
                   name = "cloudinit"
                 },
+                {
+                  disk = {
+                    bus = "virtio"
+                  }
+                  name = "mounted-disk"
+                },
               ]
               rng = {}
             }
             resources = {
               requests = {
-                memory = "1Gi"
+                memory = var.system_vm_memory
               }
             }
           }
@@ -55,35 +147,38 @@ resource "kubernetes_manifest" "ubuntu-terraform" {
               cloudInitNoCloud = {
                 userData = <<-EOT
                 #cloud-config
-                users:
-                  - name: admin
-                    sudo: ALL=(ALL) NOPASSWD:ALL
                     ssh_authorized_keys:
-                      - ssh-rsa AAAA...
+                      - ${var.system_vm_ssh}
                 EOT
               }
               name = "cloudinit"
             },
+            {
+              name = "mounted-disk"
+              persistentVolumeClaim = {
+                claimName = kubernetes_persistent_volume_claim.system_vm1_persistent_volume_claim.metadata[0].name
+              }
+            }
           ]
         }
       }
     }
   }
 }
-#==============================================================================================================================#
-#                                                                                                                              #
-#                                                     Skyfarm Server Group                                                     #
-#                                                                                                                              #
-#==============================================================================================================================#
+#==============================================================================#
+#                                                                              #
+#                              Dipal Server Group                              #
+#                                                                              #
+#==============================================================================#
 
-#==============================================================================================================================#
-#                                                                                                                              #
-#                                                    Scipaper Server Group                                                     #
-#                                                                                                                              #
-#==============================================================================================================================#
+#==============================================================================#
+#                                                                              #
+#                             Skyfarm Server Group                             #
+#                                                                              #
+#==============================================================================#
 
-#==============================================================================================================================#
-#                                                                                                                              #
-#                                                     System Server Group                                                      #
-#                                                                                                                              #
-#==============================================================================================================================#
+#==============================================================================#
+#                                                                              #
+#                            Scipaper Server Group                             #
+#                                                                              #
+#==============================================================================#
--- universe/IaC/universe/computing/variables.tf
+++ universe/IaC/universe/computing/variables.tf
@@ -1,54 +1,106 @@
-#MIT License
-#Copyright (c) 2023 Skyfarm
-#Permission is hereby granted, free of charge, to any person obtaining a copy
-#of this software and associated documentation files (the Software), to deal
-#in the Software without restriction, including without limitation the rights
-#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#copies of the Software, and to permit persons to whom the Software is
-#furnished to do so, subject to the following conditions:
-#The above copyright notice and this permission notice shall be included in all
-#copies or substantial portions of the Software.
-#THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#SOFTWARE.
-#==============================================================================================================================#
-#                                                                                                                              #
-#                                                      Dipal Server Group                                                      #
-#                                                                                                                              #
-#==============================================================================================================================#
-variable "dipal_namespace" {
-  type    = string
-  default = "dipal"
-}
-
-#==============================================================================================================================#
-#                                                                                                                              #
-#                                                     Skyfarm Server Group                                                     #
-#                                                                                                                              #
-#==============================================================================================================================#
-variable "skyfarm_namespace" {
-  type    = string
-  default = "skyfarm"
-}
-#==============================================================================================================================#
-#                                                                                                                              #
-#                                                    Scipaper Server Group                                                     #
-#                                                                                                                              #
-#==============================================================================================================================#
-variable "scipaper_namespace" {
-  type    = string
-  default = "scipaper"
-}
-#==============================================================================================================================#
-#                                                                                                                              #
-#                                                     System Server Group                                                      #
-#                                                                                                                              #
-#==============================================================================================================================#
+#==============================================================================#
+#                                                                              #
+#                                 MIT License                                  #
+#                                                                              #
+#                       Copyright (c) 2023 Skyfarm                             #
+#                                                                              #
+# Permission is hereby granted, free of charge, to any person obtaining a copy #
+#of this software and associated documentation files (the Software), to deal #
+# in the Software without restriction, including without limitation the rights #
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell   #
+#    copies of the Software, and to permit persons to whom the Software is     #
+#           furnished to do so, subject to the following conditions:           #
+#                                                                              #
+#The above copyright notice and this permission notice shall be included in all#
+#               copies or substantial portions of the Software.                #
+#                                                                              #
+#  THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR  #
+#   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,   #
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE  #
+#    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER    #
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, #
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE #
+#                                  SOFTWARE.                                   #
+#                                                                              #
+#==============================================================================#
+#==============================================================================#
+#                                                                              #
+#                             Variables Group                                  #
+#                                                                              #
+#==============================================================================#
+# defines the namespace we want to use to provision the computing machine
 variable "system_namespace" {
   type    = string
   default = "system"
+}
+#-------------------------------------------------------------------------------
+#defines the name for the computing machine
+variable "system_vm_name"{
+    type = string
+    default = "system-vm1"
+}
+#-------------------------------------------------------------------------------
+#defines the os type we want to provision the computing machine based on
+variable "system_vm_os" {
+  type = string
+  default ="ubuntu"
+}
+#-------------------------------------------------------------------------------
+# defines the address of the os container in the repository
+variable "system_vm_container" {
+  type = string
+  default = "quay.io/containerdisks/ubuntu"
+}
+#-------------------------------------------------------------------------------
+# defines version of the container image
+variable "system_vm_container_version" {
+  type = string
+  default = "22.04"
+}
+#-------------------------------------------------------------------------------
+# defines the number of CPUs we want to dedicate to the computing machine
+variable "system_vm_cpus" {
+    type = number
+    default=1  
+}
+#-------------------------------------------------------------------------------
+#defines the memory capacity we want to dedicate to the computing machine
+variable "system_vm_memory" {
+  type = string
+  default = "1Gi"
+}
+#-------------------------------------------------------------------------------
+#defines the computing machine type (container and vm)
+# container is just the application layer and uses the core of the host
+# vm is a virtual machine with a fully isolated environment and clones the core of the host
+variable "system_vm_type"{
+    type = map
+    default = {
+        container = "container"
+        vm  = "vm"
+    }
+}
+#-------------------------------------------------------------------------------
+# defines the name of the disk we want to attach to the vm
+variable "system_vm_disk_name" {
+  type = string
+  default = "containerdisk"
+}
+#-------------------------------------------------------------------------------
+# defines the disk data bus of the disk
+variable "system_vm_disk_bus" {
+  type = string
+  default = "virtio"
+}
+#-------------------------------------------------------------------------------
+# defines the size of the disk
+variable "system_vm_disk_size" {
+  type = number
+  default = 10
+}
+#-------------------------------------------------------------------------------
+# defines the use generated ssh key we want to use for ssh login to the vm
+variable "system_vm_ssh" {
+    type = string
+    default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCnKHjufDiMLojY6lKhtwh1jZCLChUq6HESUHfU0s4rANfTxdlBOhqGO0ZtWyRhta4gd/qet2+s6GF0c6US2lmoQ6GPOlC256lNb4HiM+/Ar46vgLlAnyEtUqXwILSctye69zaJ4vF82aaTLL8LxqEZMIdWbQj+afM4qHQuETDQZDjbZ//PPXpXqEUP8dnE8Ylgkj33iyCYQwRCuLq2g91jrtp6aGpUqhNDTwC1KL5mWv0kX+Sj6JXW3srLA8px3JNCNsY2DHSyDRqZdCDxX4+Q7ha8BogkcJ32ud3y3jUuzGEBrvEcjATisYMkd2G6hiCb2Lt+FFbYyqrtGIn+DU/3e736fqt5WPdEaBKTnCpuyhLe4mUTPVWPYQU0eoPgrrprHu9trXzZX7YQZiTRXveFc0gMs/+TKHS1QzyhnK9fMwJa20YrkbHDKzXkE56+wgOtAukNKP88bDpgYHCQxp1qACZOK+qz3053fI50s6nPf1Seh4LgB46IO0YR3NQLu+c= moeid@moeid-IdeaPad-L340-17IRH-Gaming" 
 }
